edb72d65032f3ebe28f4f952411a882d64239d0b
Beth Tenorio <betsky98@gmail.com>
2018-10-26 21:10:19 -0400

    allow deletion of past deadlines

--- countdown.rb
+++ countdown.rb
@@ -28,13 +28,48 @@ class Tracker
 end
 
 puts '---'
-CSV.foreach("/Users/Tenorio/vermonster/days_until/deadlines.csv") do |row|
-    if row == []
-      puts "You have no deadlines. Yay!"
-      break
-    else
-      month, day, event = row
-      Araw.new(month, day, event).display
+
+if File.file?('deadlines.csv')
+  File.open('deadlines-bak.csv', 'w') do |line|
+    CSV.foreach(File.open('deadlines.csv')) do |row|
+      if row == []
+        puts "You have no deadlines. Yay!"
+      else
+        month, day, event, year = row
+        if year.nil?
+          year = Time.now.year
+        end
+        tracker = Tracker.new(month, day, event, year)
+        tracker.display
+
+        if tracker.count_day >= 0
+          line.puts row.join(',')
+        end
+      end
     end
+  end
+  puts '---'
+  puts 'Remove all past deadlines? Enter y/n:'
+  response = gets.chomp
+  if response == 'y'
+    begin
+      File.delete('deadlines.csv')
+      File.rename('deadlines-bak.csv', 'deadlines.csv')
+      puts 'Past deadlines have been removed from your records'
+    rescue
+      puts 'Your current deadlines are saved'
+      File.rename('deadlines-bak.csv', 'deadlines.csv')
+    end
+  else
+    puts 'No changes were made to your deadlines'
+  end
+else
+  puts 'You don\'t have anything recorded yet'
+  puts 'To start logging your deadlines, type something like:'
+  puts '$ echo "3,9,launch app" >> deadlines.csv'
+  puts 'To log a deadline for a date next year:'
+  puts '$ echo "1,1,write new year\'s resolution,2019" >> deadlines.csv'
+  puts 'Then run the ruby script again'
 end
-puts '---'
+
+
